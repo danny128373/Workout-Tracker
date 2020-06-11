b0d7b39801c647414c32dcb1e4cc1cfaf422ea1d
Daniel Meza <daniel.meza29@yahoo.com>
2020-06-11 15:00:06 -0500

    Added a bit of data to workoutTracker.json

---
+++ src/WorkoutTracker.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+
+export default function WorkoutTracker() {
+
+
+  return (
+    <div>
+      WorkoutTracker
+      <img alt="benchpress" src="https://res.cloudinary.com/dp5l2gxzh/image/upload/v1591902686/8_anojap.jpg" />
+    </div>
+  )
+}
