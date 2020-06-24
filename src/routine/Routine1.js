38de5550609e3f57c5f0fca44d941cff78c33e7c
Daniel Meza <daniel.meza29@yahoo.com>
2020-06-24 14:06:05 -0500

    User can choose exercises to add to a routine

--- src/routine/Routine1.js
+++ src/routine/Routine1.js
@@ -8,47 +8,77 @@ export default function Routine1(props) {
 
   const [exercises, setExercises] = useState([])
   const [selectedExercises, setSelectedExercises] = useState([])
-  const [isChecked, setIsChecked] = useState(false)
+  const [numberOfRoutines, setNumberOfRoutines] = useState(null)
+  const [routineName, setRoutineName] = useState({ name: "" })
 
   const getExercises = () => {
     ApiManager.getAll('exercises').then(exercises => {
-      setExercises(exercises)
+      exercises.map(exercise => {
+        exercise.check = false
+        setExercises([...exercises, exercise])
+      })
     })
   }
 
-  const onChangeHandler = (event) => {
+  const routineNameHandler = (event) => {
+    const stateToChange = { ...routineName }
+    stateToChange.name = event.target.value
+    setRoutineName(stateToChange)
+  }
+
+  const onChangeHandler = (e, exercise) => {
     const stateToChange = [...selectedExercises]
-    if (!isChecked) {
-      ApiManager.get('exercises', event.target.id).then(exercise => {
-        setSelectedExercises([...stateToChange, exercise])
-        setIsChecked(true)
-      })
+    if (exercise.check) {
+      exercise.check = false
+      setSelectedExercises(stateToChange)
     } else {
-      const newArray = stateToChange.filter(exercise => exercise.id === event.target.id + 1)
-      setIsChecked(false)
-      setSelectedExercises([...newArray])
+      exercise.check = true
+      if (!stateToChange.includes(exercise)) {
+        setSelectedExercises([...stateToChange, exercise])
+      } else {
+        setSelectedExercises(stateToChange)
+      }
     }
   }
+
+  const getRoutines = () => {
+    ApiManager.getAll('routines').then(e => {
+      setNumberOfRoutines(e.length)
+    })
+  }
+
+  const onSubmitHandler = () => {
+    const trueExercisesArray = [...selectedExercises]
+    const newArray = trueExercisesArray.filter(exercise => exercise.check === true)
+    ApiManager.post({ name: routineName.name, userId: JSON.parse(sessionStorage.getItem('credentials'))[0].id }, 'routines').then(e => {
+      newArray.map(exercise => {
+        ApiManager.post({ routineId: e.id, userId: JSON.parse(sessionStorage.getItem('credentials'))[0].id, exerciseId: exercise.id }, 'routineExercises')
+        props.history.push("/newWorkout")
+      })
+    })
+  }
+
+  useEffect(getRoutines, [])
 
   useEffect(getExercises, [])
 
   return (
     <div className="exerciseRoutineListContainer">
-      <input type="text" placeholder="Workout Plan Name" />
+      <input onChange={routineNameHandler} type="text" id="routineName" placeholder="Workout Plan Name" />
       {exercises.map(exercise => {
         return (
-          <Table className="exerciseListSelectorForRoutines" key={exercise.id}>
+          <Table className="exerciseListSelectorForRoutines">
             <tbody>
               <tr>
                 <td><img src={exercise.url} /></td>
                 <td className="exerciseNameTdRoutine">{exercise.name}</td>
-                <td><input defaultChecked={isChecked} type="checkbox" id={exercise.id} onChange={onChangeHandler} value="Add" /></td>
+                <td><input defaultChecked={false} type="checkbox" id={exercise.id} onChange={e => onChangeHandler(e, exercise)} value="Add" /></td>
               </tr>
             </tbody>
           </Table>
         )
       })}
-      <Button className="exerciseRoutineListButton">Add exercises</Button>
+      <Button onClick={onSubmitHandler} className="exerciseRoutineListButton">Add exercises</Button>
     </div>
   )
 }
